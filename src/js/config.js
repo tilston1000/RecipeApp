59426cb825c984063d92bd4432b50bbaa78c23ad
tilston1000 <info@stratosphereqa.com>
2025-10-29 23:00:03 +0000

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = '54810f53-320a-4da9-ac92-5a812da428f8';
+export const MODAL_CLOSE_SEC = 2.5;
